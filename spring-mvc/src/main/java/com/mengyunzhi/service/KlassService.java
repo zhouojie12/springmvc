b2b648370c97c916d487d45aaaec57fe8b40fdac
周杰 <2728774085@qq.com>
2017-07-27 10:17:03 +0800

    7.27

--- spring-mvc/src/main/java/com/mengyunzhi/service/KlassService.java
+++ spring-mvc/src/main/java/com/mengyunzhi/service/KlassService.java
@@ -6,7 +6,6 @@ import com.mengyunzhi.repository.Klass;
  * Created by Administrator on 2017/7/26.
  */
 public interface KlassService {
-    Klass add(String name, Long teacherId);
     Klass get(Long id);
     void delete(Long id);
     Klass update(Long id, String name, Long teacherId);
--- spring-mvc/src/main/java/com/mengyunzhi/service/KlassServiceImpl.java
+++ spring-mvc/src/main/java/com/mengyunzhi/service/KlassServiceImpl.java
@@ -1,6 +1,10 @@
 package com.mengyunzhi.service;
 
 import com.mengyunzhi.repository.Klass;
+import com.mengyunzhi.repository.KlassRepository;
+import com.mengyunzhi.repository.Teacher;
+import com.mengyunzhi.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,28 +12,50 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class KlassServiceImpl implements KlassService {
-    @Override
-    public Klass add(String name, Long teacherId) {
-        return null;
-    }
+    @Autowired
+    private KlassRepository klassRepository;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
 
     @Override
     public Klass get(Long id) {
-        return null;
+        return klassRepository.findOne(id);
     }
 
     @Override
     public void delete(Long id) {
-
+        klassRepository.delete(id);
+        return;
     }
 
     @Override
     public Klass update(Long id, String name, Long teacherId) {
-        return null;
+
+        // 实例化班级，赋班级名称
+        Klass klass = klassRepository.findOne(id);
+        klass.setName(name);
+
+        // 实例化传入的教师，设置教师
+        Teacher teacher = teacherRepository.findOne(teacherId);
+        klass.setTeacher(teacher);
+
+        return klassRepository.save(klass);
     }
 
     @Override
     public Klass save(String name, Long teacherId) {
-        return null;
+        // 实例化，并设置班级名称
+        Klass klass = new Klass();
+        klass.setName(name);
+
+        // 获取教师
+        Teacher teacher = teacherRepository.findOne(teacherId);
+
+        // 设置教师
+        klass.setTeacher(teacher);
+
+        // 保存
+        return klassRepository.save(klass);
     }
 }
